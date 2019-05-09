1bc0178f6aea6a9b90320ae439525a70c1252303
Joao Lopes <joaolopes71@hotmail.com>
2019-05-08 22:16:57 -0300

    initial commit and modules 1, 2, 3, 4 and 5

---
+++ src/main/java/com/costalopes/apachekafkastudy/main/KafkaProducerApp.java
@@ -0,0 +1,45 @@
+package com.costalopes.apachekafkastudy.main;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class KafkaProducerApp {
+
+    public static void main(String[] args) {
+
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092, localhost:9093");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        //
+        // instanciando o producer
+        //
+
+        // exemplo de record com propriedades opcionais setadas no construtor
+        @SuppressWarnings("unused")
+        ProducerRecord optionalProperties = new ProducerRecord("my_topic", 1, 1234545454425L, "Course-001", "My Message Opcional");
+
+        //
+        // instanciando records em um for e mandando para o topico
+        //
+        try (KafkaProducer<String, String> myProducer = new KafkaProducer<>(props)) {
+
+            for (int i = 0; i < 150; i++) {
+
+                myProducer.send(new ProducerRecord("my_topic", Integer.toString(i), "My Message " + Integer.toString(i)));
+
+            }
+
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+}
